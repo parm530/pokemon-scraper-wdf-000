e436287044f08854f412bea87312b0c31a1762d9
parm530 <parmsahadeo530@gmail.com>
2016-08-17 16:05:20 -0400

    Added methods to Pokemon class using a database

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id, name, type, db)
+    @id, @name, @type, @db = id, name, type, db
+  end
+
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find(id, database_connection)
+    database_connection.execute("SELECT id, name, type FROM pokemon WHERE id = ?", id)
+  end
+  
 end
